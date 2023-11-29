0b33f6a7e9f08972caecec2b25ce8c86526ed1b7
MorskoySom <Somovvitvit@gmail.com>
2023-11-29 16:30:20 +0200

    f16

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,4 +1,19 @@
-import React, {useReducer} from "react";
+import { type } from "@testing-library/user-event/dist/type";
+import React, { useReducer } from "react";
+
+type RequestStep = 'idle' | 'start' | 'pending' | 'finished';
+
+interface State {
+  isRequestInProgress: boolean;
+  requestStep: RequestStep;
+};
+
+type Action =
+  { type: 'START_REQUEST' }
+  | { type: 'PENDING_REQUEST' }
+  | { type: 'FINISH_REQUEST' }
+  | { type: 'RESET_REQUEST' };
+  
 
 const initialState: State = {
   isRequestInProgress: false,
